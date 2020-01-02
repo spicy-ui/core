7ea9134034d87c2ff2e2dc6ed13e719b58e329b7
Alex Gabites <alex@netspeed.net.nz>
2020-01-02 18:24:09 +1300

    remove additional props from box

--- src/Box/Box.tsx
+++ src/Box/Box.tsx
@@ -30,17 +30,9 @@ export interface BoxProps
     BackgroundProps,
     ColorProps,
     TypographyProps,
-    BorderProps {
-  /** Additional CSS classes to add to the component. */
-  className?: string;
-  /** Additional CSS properties to add to the component. */
-  style?: React.CSSProperties;
-}
+    BorderProps {}
 
-/**
- * Box is a view with all styled-system hooks added to it. You can use it as a base component for all display elements.
- */
-const Box = styled.div<BoxProps>`
+const Box = styled('div')<BoxProps>`
   ${layout}
   ${position}
   ${flexbox}
