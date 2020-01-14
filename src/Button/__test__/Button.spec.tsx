7e8cfa08a9ec6abbce3beb91f1f50c1000ad77c3
Alex Gabites <alex@netspeed.net.nz>
2020-01-14 23:41:48 +1300

    fix button test

--- src/Button/__test__/Button.spec.tsx
+++ src/Button/__test__/Button.spec.tsx
@@ -1,11 +1,17 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Button } from '../';
+import { Theme } from '../../Theme';
 
 describe('button', () => {
   it('renders', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Button>a button</Button>, div);
+    ReactDOM.render(
+      <Theme>
+        <Button>a button</Button>
+      </Theme>,
+      div,
+    );
     ReactDOM.unmountComponentAtNode(div);
   });
 });
