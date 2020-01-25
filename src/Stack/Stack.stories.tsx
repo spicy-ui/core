f02d0e71b2d3d547b4cbfbd1a40af01cd875c1a3
Alex Gabites <alex@netspeed.net.nz>
2020-01-25 19:53:41 +1300

    improve stack stories

--- src/Stack/Stack.stories.tsx
+++ src/Stack/Stack.stories.tsx
@@ -9,13 +9,13 @@ export default {
 
 export const Vertical = () => (
   <Stack spacing="base">
-    <Box p="xs" border="1px solid" borderColor="gray.200" borderRadius="sm">
+    <Box p="xs" bg="red.50">
       Box 1
     </Box>
-    <Box p="xs" border="1px solid" borderColor="gray.200" borderRadius="sm">
+    <Box p="xs" bg="green.50">
       Box 2
     </Box>
-    <Box p="xs" border="1px solid" borderColor="gray.200" borderRadius="sm">
+    <Box p="xs" bg="blue.50">
       Box 3
     </Box>
   </Stack>
@@ -23,13 +23,13 @@ export const Vertical = () => (
 
 export const Horizontal = () => (
   <Stack orientation="horizontal" spacing="base">
-    <Box p="xs" border="1px solid" borderColor="gray.200" borderRadius="sm">
+    <Box p="xs" bg="red.50">
       Box 1
     </Box>
-    <Box p="xs" border="1px solid" borderColor="gray.200" borderRadius="sm">
+    <Box p="xs" bg="green.50">
       Box 2
     </Box>
-    <Box p="xs" border="1px solid" borderColor="gray.200" borderRadius="sm">
+    <Box p="xs" bg="blue.50">
       Box 3
     </Box>
   </Stack>
@@ -37,13 +37,13 @@ export const Horizontal = () => (
 
 export const Reversed = () => (
   <Stack isReversed spacing="base">
-    <Box p="xs" border="1px solid" borderColor="gray.200" borderRadius="sm">
+    <Box p="xs" bg="red.50">
       Box 1
     </Box>
-    <Box p="xs" border="1px solid" borderColor="gray.200" borderRadius="sm">
+    <Box p="xs" bg="green.50">
       Box 2
     </Box>
-    <Box p="xs" border="1px solid" borderColor="gray.200" borderRadius="sm">
+    <Box p="xs" bg="blue.50">
       Box 3
     </Box>
   </Stack>
@@ -51,13 +51,13 @@ export const Reversed = () => (
 
 export const HorizontalReversed = () => (
   <Stack orientation="horizontal" isReversed spacing="base">
-    <Box p="xs" border="1px solid" borderColor="gray.200" borderRadius="sm">
+    <Box p="xs" bg="red.50">
       Box 1
     </Box>
-    <Box p="xs" border="1px solid" borderColor="gray.200" borderRadius="sm">
+    <Box p="xs" bg="green.50">
       Box 2
     </Box>
-    <Box p="xs" border="1px solid" borderColor="gray.200" borderRadius="sm">
+    <Box p="xs" bg="blue.50">
       Box 3
     </Box>
   </Stack>
