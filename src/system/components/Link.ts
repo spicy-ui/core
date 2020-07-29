a37bfc1917bda6787e1ffe3f8fa272c2f245d846
Alex Gabites <alex@netspeed.net.nz>
2020-07-29 18:13:22 +1200

    link style changes

--- src/system/components/Link.ts
+++ src/system/components/Link.ts
@@ -3,23 +3,29 @@ import { ComponentTheme } from '../types';
 
 export const Link: ComponentTheme = {
   baseStyle: (props: any) => ({
-    color: 'inherit',
+    color: 'blue.500',
     cursor: 'pointer',
+    fontWeight: 'medium',
     outline: 'none',
     textDecoration: 'none',
     transitionProperty: get(props.theme, 'transitions.property.common'),
     transitionDuration: get(props.theme, 'transitions.duration.150'),
     transitionTimingFunction: get(props.theme, 'transitions.timing.out'),
     ':hover': {
+      color: 'blue.600',
       textDecoration: 'underline',
+    },
+    ':active': {
+      color: 'blue.700',
     },
     ':focus': {
       boxShadow: 'outline',
     },
     '&[aria-disabled="true"], &[aria-disabled="true"]:focus, &[aria-disabled="true"]:hover': {
       opacity: 0.4,
-      cursor: 'not-allowed',
       boxShadow: 'none',
+      cursor: 'not-allowed',
+      textDecoration: 'none',
     },
   }),
 };
