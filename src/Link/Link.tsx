5c4a6d6138166ade91f4384a1f9302dccc600e4d
Alex Gabites <alex@netspeed.net.nz>
2020-07-29 19:48:47 +1200

    rework link component again

--- src/Link/Link.tsx
+++ src/Link/Link.tsx
@@ -1,11 +1,6 @@
-import shouldForwardProp from '@styled-system/should-forward-prop';
 import * as React from 'react';
 import styled from 'styled-components';
 import { baseStyle, withColorMode } from '../util';
-
-const StyledLink = styled('a').withConfig<LinkProps>({ shouldForwardProp })`
-  ${withColorMode(baseStyle('components.Link'))}
-`;
 
 export interface LinkProps extends Partial<React.AnchorHTMLAttributes<HTMLAnchorElement>> {
   /** Set to `true` to disable the link. */
@@ -14,36 +9,24 @@ export interface LinkProps extends Partial<React.AnchorHTMLAttributes<HTMLAnchor
   isExternal?: boolean;
 }
 
-const InnerLink: React.FC<LinkProps> = ({ children, isExternal, isDisabled, onClick, ...props }) => {
-  const handleClick = React.useCallback(
-    (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-      if (isDisabled) {
-        e.preventDefault();
-        return;
-      }
+const Link = styled('a').attrs(({ isDisabled, isExternal, onClick }: LinkProps) => ({
+  'aria-disabled': isDisabled || undefined,
+  tabIndex: isDisabled ? -1 : undefined,
+  target: isExternal ? '_blank' : undefined,
+  role: isExternal ? `noopener noreferrer` : undefined,
+  onClick: (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    if (isDisabled) {
+      e.preventDefault();
+      return;
+    }
 
-      if (onClick) {
-        onClick(e);
-      }
-    },
-    [isDisabled, onClick],
-  );
-
-  return (
-    <StyledLink
-      aria-disabled={isDisabled || undefined}
-      tabIndex={isDisabled ? -1 : undefined}
-      target={isExternal ? '_blank' : undefined}
-      role={isExternal ? `noopener noreferrer` : undefined}
-      onClick={handleClick}
-      {...props}
-    >
-      {children}
-    </StyledLink>
-  );
-};
-
-const Link = styled(InnerLink)``;
+    if (onClick) {
+      onClick(e);
+    }
+  },
+}))<LinkProps>`
+  ${withColorMode(baseStyle('components.Link'))}
+`;
 
 Link.displayName = 'Link';
 
