44720b7ad493724d3500d4e4af42186d6b77adf7
Alex Gabites <alex@netspeed.net.nz>
2020-10-26 22:38:55 +1300

    better shadow elevations

--- src/system/foundations/shadows.ts
+++ src/system/foundations/shadows.ts
@@ -3,13 +3,11 @@ import { colors } from './colors';
 
 export const shadows = {
   none: 'none',
-  xs: '0 0 0 1px rgba(0, 0, 0, 0.05)',
-  sm: '0 1px 2px 0 rgba(0, 0, 0, 0.05)',
-  md: '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
-  lg: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
-  xl: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
-  '2xl': '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
-  '3xl': '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
+  xs: '0 0 0 1px rgba(0, 0, 0, 0.15)',
+  sm: '0px 2px 6px rgba(0, 0, 0, 0.15)',
+  md: '0px 6px 12px rgba(0, 0, 0, 0.15)',
+  lg: '0px 12px 24px rgba(0, 0, 0, 0.15)',
+  xl: '0px 24px 48px rgba(0, 0, 0, 0.15)',
   inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
   outline: `0 0 0 3px ${rgba(colors.blue[500], 0.5)}`,
   warning: `0 0 0 3px ${rgba(colors.orange[500], 0.5)}`,
