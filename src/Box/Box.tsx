5435faacbbf897139741a70f1f95deaf127a59c2
Alex Gabites <alex@netspeed.net.nz>
2020-10-27 20:33:20 +1300

    fix zIndex prop on box not allowing string

--- src/Box/Box.tsx
+++ src/Box/Box.tsx
@@ -1,4 +1,5 @@
 import shouldForwardProp from '@styled-system/should-forward-prop';
+import * as CSS from 'csstype';
 import styled from 'styled-components';
 import {
   background,
@@ -16,6 +17,8 @@ import {
   LayoutProps,
   position,
   PositionProps,
+  RequiredTheme,
+  ResponsiveValue,
   shadow,
   ShadowProps,
   space,
@@ -26,7 +29,7 @@ import {
 
 export interface BoxProps
   extends LayoutProps,
-    PositionProps,
+    Omit<PositionProps, 'zIndex'>,
     FlexboxProps,
     GridProps,
     SpaceProps,
@@ -34,7 +37,15 @@ export interface BoxProps
     Omit<ColorProps, 'color'>,
     TypographyProps,
     BorderProps,
-    ShadowProps {}
+    ShadowProps {
+  /**
+   * The z-index CSS property sets the z-order of a positioned element and its descendants or
+   * flex items. Overlapping elements with a larger z-index cover those with a smaller one.
+   *
+   * [MDN reference](https://developer.mozilla.org/en-US/docs/Web/CSS/z-index)
+   */
+  zIndex?: ResponsiveValue<CSS.Property.ZIndex | string, RequiredTheme>;
+}
 
 export const boxMixin = compose(layout, position, flexbox, grid, space, background, color, typography, border, shadow);
 
