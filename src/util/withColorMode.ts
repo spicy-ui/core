d35639540c69cdddc9a3f07a67da1ac977b780d8
Alex Gabites <alex@netspeed.net.nz>
2020-10-27 20:30:17 +1300

    improve eslint config

--- src/util/withColorMode.ts
+++ src/util/withColorMode.ts
@@ -1,5 +1,6 @@
 import { useColorMode } from '../ColorModeProvider';
 
+// eslint-disable-next-line @typescript-eslint/ban-types
 export const withColorMode = (fn: Function) => (p: any) => {
   const { colorMode } = useColorMode();
   return fn({ ...p, colorMode });
