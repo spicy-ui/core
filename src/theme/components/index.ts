c382c40b19dbd51b144fb5170a12e186fd42bd74
Alex Gabites <alex@netspeed.net.nz>
2020-11-06 18:33:46 +1300

    add overlay component

--- src/theme/components/index.ts
+++ src/theme/components/index.ts
@@ -1,9 +1,11 @@
 import { Button } from './Button';
 import { Heading } from './Heading';
+import { Overlay } from './Overlay';
 import { Text } from './Text';
 
 export const components = {
   Button,
   Heading,
+  Overlay,
   Text,
 };
