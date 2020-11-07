17bdb2b412ff89cf6d315db524009cfb250ce4a0
Alex Gabites <alex@netspeed.net.nz>
2020-11-07 16:10:21 +1300

    add improved avatar component

---
+++ src/components/Avatar/index.stories.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import { uid } from 'react-uid';
+import { Avatar, Stack } from '..';
+
+const names = ['Dan Abramov', 'Ryan Florence', 'Resi Respati', 'Alex Gabites', undefined, 'John Brown', 'Alan Sega'];
+
+const avatarSizes = ['xs', 'sm', 'md', 'lg', 'xl', '2xl', '3xl'];
+
+export default {
+  title: 'Avatar',
+  component: Avatar,
+  argTypes: {
+    bg: { type: 'string' },
+  },
+};
+
+export const Simple = (props: any) => <Avatar {...props} />;
+Simple.args = {
+  name: 'Dan Abramov',
+  src: 'https://bit.ly/dan-abramov',
+};
+
+export const Sizes = (props: any) => (
+  <Stack orientation="horizontal" spacing={2}>
+    {avatarSizes.map((size, idx) => (
+      <Avatar key={uid(size, idx)} {...props} size={size as any} name={names[idx]} />
+    ))}
+  </Stack>
+);
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
+export * from './Avatar';
 export * from './Box';
 export * from './Button';
 export * from './Divider';
