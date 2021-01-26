547aefb2b9b5df95d5f43260e4e105bb39a1b2d0
Alex Gabites <alex@netspeed.net.nz>
2021-01-26 23:14:25 +1300

    update transition util timing prop

--- src/system/util/transition.ts
+++ src/system/util/transition.ts
@@ -51,7 +51,7 @@ const config: Config = {
   },
   transitionTiming: {
     property: 'transitionTimingFunction',
-    scale: 'transitions.timing',
+    scale: 'transitions.timingFn',
   },
   transitionDelay: {
     property: 'transitionDelay',
