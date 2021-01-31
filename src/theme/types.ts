f430ac0f40fcf9759b8cc5fd84b1cc3021613013
Alex Gabites <alex@netspeed.net.nz>
2021-01-31 15:58:02 +1300

    fix theme types

--- src/theme/types.ts
+++ src/theme/types.ts
@@ -1,10 +1,10 @@
-import { SystemStyleObject } from '@styled-system/css';
-
-export type ComponentThemeScale = SystemStyleObject;
+// FIXME was SystemStyleObject
+export type ComponentThemeScale = any;
 
 export interface ComponentThemeConfig {
+  // todo add generic for props
   propToScaleMap?: Array<[string, string]>;
-  defaultProps?: { [prop: string]: any };
+  defaultProps?: { [prop: string]: any }; // todo remove
   baseStyle?: ComponentThemeScale;
   [scale: string]: ComponentThemeScale | undefined;
 }
