de6909af0673ab31e7633556982f3f7ab5fa310b
Alex Gabites <alex@netspeed.net.nz>
2021-02-08 15:14:29 +1300

    add is centered prop

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -3,10 +3,18 @@ import { Box, BoxProps } from '../Box';
 
 export interface ContainerProps extends BoxProps {
   children?: React.ReactNode;
+  isCentered?: boolean;
 }
 
-export const Container = React.forwardRef<HTMLDivElement, ContainerProps>(({ children, ...rest }, ref) => (
-  <Box ref={ref} mx="auto" px={4} w="full" {...rest}>
+export const Container = React.forwardRef<HTMLDivElement, ContainerProps>(({ children, isCentered, ...rest }, ref) => (
+  <Box
+    ref={ref}
+    mx="auto"
+    px={4}
+    w="full"
+    {...(isCentered ? { d: 'flex', flexDirection: 'column', alignItems: 'center' } : {})}
+    {...rest}
+  >
     {children}
   </Box>
 ));
